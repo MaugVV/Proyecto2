8b85639699d26af59f08cfaa8c095cc6819ff731
dell <dell@DESKTOP-T9BMC37>
2017-06-07 09:30:02 -0500

    Guardar y Consultar Inventario

--- src/Formularios/Frm_Inventario_Buscar.java
+++ src/Formularios/Frm_Inventario_Buscar.java
@@ -254,12 +254,7 @@ public class Frm_Inventario_Buscar extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
+        //<
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
